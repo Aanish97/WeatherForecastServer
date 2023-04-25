cc84f7d1ea6278b9ee03979b05bb6b20761eca3f
Aanish97 <aanishamir@gmail.com>
2023-04-26 02:30:35 +0500

    add check to prevent index error

--- app.py
+++ app.py
@@ -1,46 +1,3 @@
-#from flask import Flask, render_template, jsonify, request, url_for
-#from shapely.geometry import Point as Shapely_point, mapping
-#from geojson import Point as Geoj_point, Polygon as Geoj_polygon, Feature, FeatureCollection
-#from datetime import datetime
-#from sqlalchemy import *
-#import pandas as pd
-#import geopandas as gpd
-#import numpy as np
-#import psycopg2 as pg
-#import json 
-#import leaflet as L
-#from elastic_app_search import Client
-#from elasticsearch import Elasticsearch
-#from elasticapm.contrib.flask import ElasticAPM
-#import matplotlib.colors as cl
-#import h3
-#import h3.api.basic_int as h3int
-#import json
-#import h3pandas
-#import cmasher as cmr
-#import plotly
-#import plotly.express as px
-#from scipy.stats import percentileofscore
-#from scipy import stats
-#import plotly.graph_objects as go
-#import os 
-#import datetime
-#from netCDF4 import Dataset
-#import shapely.wkt
-#import folium
-#import ftplib
-#from ftplib import FTP
-#from pathlib import Path
-#from os import path, walk
-
-#import timezonefinder, pytz
-#import plotly.express as px
-#import plotly.graph_objects as go
-#from geopy.geocoders import Nominatim
-#import math
-
-#from models import *
-
 from flask import Flask, render_template, jsonify, request, url_for
 from datetime import datetime
 import pandas as pd
@@ -53,7 +10,6 @@ from netCDF4 import Dataset
 import timezonefinder, pytz
 import plotly.express as px
 from geopy.geocoders import Nominatim
-from dotenv import load_dotenv
 
 
 
@@ -167,54 +123,55 @@ var_val3D = []
 var_val4D = []
 #NOTE: the variable are in opposite order var_val4D[lat, lon, forecast_time_index, 0/1/2/3, where 0=CRAIN, 1=SOILW... etc]
 
-updatedDtStr = list_of_ncfiles[0].split('__')[0]
-updatedDt = datetime.datetime.strptime(updatedDtStr,'%Y%m%d_%H%M%S')
-updatedDtDisplay = datetime.datetime.strftime(updatedDt, '%Y-%m-%dT%H%M%S')
+if len(list_of_ncfiles) > 0:
+	updatedDtStr = list_of_ncfiles[0].split('__')[0]
+	updatedDt = datetime.datetime.strptime(updatedDtStr,'%Y%m%d_%H%M%S')
+	updatedDtDisplay = datetime.datetime.strftime(updatedDt, '%Y-%m-%dT%H%M%S')
 
-#get the forecast end dt
-forecastEndDtStr = list_of_ncfiles[-1].split('__')[1].split('__')[0]
-forecastEndDt = datetime.datetime.strptime(forecastEndDtStr,'%Y%m%d_%H%M%S')
-forecastEndDtDisplay = datetime.datetime.strftime(forecastEndDt, '%Y-%m-%dT%H%M%S')
+	#get the forecast end dt
+	forecastEndDtStr = list_of_ncfiles[-1].split('__')[1].split('__')[0]
+	forecastEndDt = datetime.datetime.strptime(forecastEndDtStr,'%Y%m%d_%H%M%S')
+	forecastEndDtDisplay = datetime.datetime.strftime(forecastEndDt, '%Y-%m-%dT%H%M%S')
 
-i=0
-for varName in varList:
-	tm_arr = []
-	print('Reading data for: '+ varName)
-	j=0
-	for f in list_of_ncfiles:
-		#f = '20211209_000000__20211212_210000__093___gfs.t00z.pgrb2.0p25.f093.grb2.nc'
-		
-		ncin = Dataset(outDir+f, "r")
+	i=0
+	for varName in varList:
+		tm_arr = []
+		print('Reading data for: '+ varName)
+		j=0
+		for f in list_of_ncfiles:
+			#f = '20211209_000000__20211212_210000__093___gfs.t00z.pgrb2.0p25.f093.grb2.nc'
 
-		titleStr = varDict[varName]
-		var_mat = ncin.variables[varName][:]
+			ncin = Dataset(outDir+f, "r")
 
-		if 'Temp' in titleStr:
-			var_val = var_mat.squeeze() - 273.15 #convert to DegC
-		elif 'Precipitation Rate' in titleStr:
-			var_val = var_mat.squeeze() * 3600 #convert to mm/hr
+			titleStr = varDict[varName]
+			var_mat = ncin.variables[varName][:]
+
+			if 'Temp' in titleStr:
+				var_val = var_mat.squeeze() - 273.15 #convert to DegC
+			elif 'Precipitation Rate' in titleStr:
+				var_val = var_mat.squeeze() * 3600 #convert to mm/hr
+			else:
+				var_val = var_mat.squeeze()
+
+			lons = ncin.variables['longitude'][:]
+			lats = ncin.variables['latitude'][:]
+			tms = ncin.variables['time'][:]
+			#tmstmpStr = datetime.datetime.fromtimestamp(tm.data[0]).strftime('%Y%m%d%H%M%S')
+
+			if j>0:
+				var_val3D = np.dstack((var_val3D,var_val.data))
+			else:
+				var_val3D = var_val.data
+			tm_arr.append(tms.data[0])
+
+			ncin.close()
+			j=j+1
+		if i>0:
+			var_val3D_rshp = np.reshape(var_val3D , (720,1440,time_dim,1))
+			var_val4D = np.append( var_val3D_rshp , var_val4D , axis = 3)
 		else:
-			var_val = var_mat.squeeze()
-
-		lons = ncin.variables['longitude'][:]
-		lats = ncin.variables['latitude'][:]
-		tms = ncin.variables['time'][:]
-		#tmstmpStr = datetime.datetime.fromtimestamp(tm.data[0]).strftime('%Y%m%d%H%M%S')
-
-		if j>0:
-			var_val3D = np.dstack((var_val3D,var_val.data))
-		else:
-			var_val3D = var_val.data
-		tm_arr.append(tms.data[0])
-
-		ncin.close()
-		j=j+1
-	if i>0:
-		var_val3D_rshp = np.reshape(var_val3D , (720,1440,time_dim,1))
-		var_val4D = np.append( var_val3D_rshp , var_val4D , axis = 3)
-	else:
-		var_val4D = np.reshape(var_val3D , (720,1440,time_dim,1))
-	i=i+1
+			var_val4D = np.reshape(var_val3D , (720,1440,time_dim,1))
+		i=i+1
 
 def fixToLocalTime(df,lat,lon):
 	tf = timezonefinder.TimezoneFinder()
